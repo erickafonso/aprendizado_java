6bc237dcb49616bfd3cc8bc47cff8a3586cc319c
Erick Afonso <36458464+erickafonso@users.noreply.github.com>
2023-09-21 09:55:56 -0300

    Add files via upload

---
+++ Lista_de_exercicios.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lista_de_exercicios;
+
+/**
+ *
+ * @author 182310022
+ */
+import java.util.Scanner;
+public class Lista_de_exercicios {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        // 1. Calcular e mostrar a metade de um número. 
+        /*
+        System.out.println("Hello World!");
+        System.out.println("Digite un numero: ");
+        int numero = ler.nextInt();
+        int metade = numero/2;
+        System.out.println("A metade no número é: " + metade);
+        */
+        
+        
+        //2. Calcular a idade de uma pessoa, sabendo-se o ano de nascimento.
+        /*
+        System.out.println("Digite o seu ano de nascimento: ");
+        int ano_nasc = ler.nextInt();
+        System.out.println("Sua idade é: " + (2023 - ano_nasc));
+        */
+        
+        
+        /*3. Calcular e mostrar o valor da área de um retângulo dado o 
+           valor dos dois lados. A área do retângulo é calculada pela fórmula areaRetangulo = lado1 * lado2
+          
+        System.out.println("Informe o primeiro lado do retângulo: ");
+        int lado1 = ler.nextInt();
+        System.out.println("Informe o segundo lado do retângulo: ");
+        int lado2 = ler.nextInt();
+        int areaRetangulo = lado1 * lado2;
+        System.out.println("A area do retângulo é: "+ areaRetangulo);
+        */ 
+        
+        /* 4. Calcular e mostrar o valor do perímetro de
+              um retângulo dado o valor dos dois lados. O perímetro do retângulo é calculado pela fórmula 
+              perimetroRetangulo = 2*lado1 + 2*lado2
+         
+        System.out.println("Digite o primeiro lado do retângulo: ");
+        int Ex4Lado1 = ler.nextInt();
+        System.out.println("Digite o segundo lado do retângulo: ");
+        int Ex4Lado2 = ler.nextInt();
+        int perimetroRetangulo = 2*Ex4Lado1 + 2*Ex4Lado2;
+        System.out.println("O perímetro do retângulo é " + perimetroRetangulo);
+        */
+        
+        /* 5. Uma loja está fazendo liquidação, vendendo tudo por 40% do preço original. Faça um programa em 
+            Java que calcule o valor do desconto, dado o preço original do produto. 
+        
+        System.out.println("A loja BLABLABLA está oferecendo 40% em todos os produtos!!!");
+        System.out.println("Digite o valor do produto que você quer saber o preço final: R$ ");
+        float Ex5precoIni = ler.nextFloat();
+        float Ex5precoFin = Ex5precoIni - ((Ex5precoIni * 40)/100); 
+        System.out.println("O preço do seu produto é: R$ "+ Ex5precoFin);
+        */
+        
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+
+    }
+}
